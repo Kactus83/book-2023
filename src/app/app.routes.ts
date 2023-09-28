50a7ee53cb42945598bb6a44bc3bd9bc2eb8fe30
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-09-28 22:18:07 +0200

    WIP routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import { Route } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+
+    // Redirect empty path to '/example'
+    {path: '', pathMatch : 'full', redirectTo: 'example'},
+
+    // Redirect signed-in user to the '/example'
+    //
+    // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'example'},
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [],
+        canActivateChild: [],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+        ]
+    }
+];
