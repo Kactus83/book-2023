32173bc37b6074023814c37bb69744b753886213
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-09-28 22:13:21 +0200

    Add EmptyLayout

--- src/app/layout/layouts/empty-layout/empty-layout.component.ts
+++ src/app/layout/layouts/empty-layout/empty-layout.component.ts
@@ -1,15 +1,38 @@
 import { NgIf } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subject } from 'rxjs';
+import { LoadingBarComponent } from 'src/app/core/components/loading-bar/loading-bar.component';
 
 @Component({
-  selector: 'app-empty-layout',
-  templateUrl: './empty-layout.component.html',
-  encapsulation: ViewEncapsulation.None,
-  standalone   : true,
-  imports      : [NgIf, RouterOutlet],
-  styleUrls: ['./empty-layout.component.scss']
+    selector     : 'empty-layout',
+    templateUrl  : './empty-layout.component.html',
+    encapsulation: ViewEncapsulation.None,
+    standalone   : true,
+    imports      : [LoadingBarComponent, NgIf, RouterOutlet],
 })
-export class EmptyLayoutComponent {
+export class EmptyLayoutComponent implements OnDestroy
+{
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
 
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void
+    {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
 }
