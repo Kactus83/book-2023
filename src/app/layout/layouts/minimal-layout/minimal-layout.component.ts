46ca33029f724ebd0644ca68b4bca0d2bc4d011f
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-03 15:22:50 +0200

    Add html for minimal layout component

--- src/app/layout/layouts/minimal-layout/minimal-layout.component.ts
+++ src/app/layout/layouts/minimal-layout/minimal-layout.component.ts
@@ -1,13 +1,14 @@
 import { NgIf } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingBarComponent } from 'src/app/core/components/loading-bar/loading-bar.component';
 
 @Component({
   selector: 'minimal-layout',
   templateUrl: './minimal-layout.component.html',
   encapsulation: ViewEncapsulation.None,
   standalone   : true,
-  imports      : [NgIf, RouterOutlet],
+  imports      : [NgIf, RouterOutlet, LoadingBarComponent],
   styleUrls: ['./minimal-layout.component.scss']
 })
 export class MinimalLayoutComponent {
